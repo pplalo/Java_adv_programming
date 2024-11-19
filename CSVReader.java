f913cc4dfe48c9b36f44197e0e66c43c2b63889b
pplalo <jose.eduardo.castagno@gmail.com>
2024-11-19 12:52:07 -0600

    Display columns in terminal to let the user select which columns wants to be shown and analysed

--- CSVReader.java
+++ CSVReader.java
@@ -1,23 +1,48 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class CSVReader {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Welcome!");
-        System.out.println("Enter route path where it is stored CSV file:");
-
-        String filePath = scanner.nextLine();
-        File file = new File(filePath);
-
-        if (file.exists() && file.isFile() && filePath.endsWith(".csv")) {
-            System.out.println("Archivo CSV encontrado: " + file.getAbsolutePath());
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Welcome!");
+            System.out.println("Enter the path where the CSV file is stored:");
             
-        } else {
-            System.out.println("Error: El archivo no existe, no es un archivo válido o no tiene extensión .csv.");
+            String filePath = scanner.nextLine();
+            File file = new File(filePath);
+            
+            if (file.exists() && file.isFile() && filePath.endsWith(".csv")) {
+                System.out.println("CSV file found: " + file.getAbsolutePath());
+                System.out.println("\nReading the available columns in the file...\n");
+                printCsvColumns(file);
+            } else {
+                System.out.println("Error: The file does not exist, is not a valid file, or does not have a .csv extension.");
+            }
+        } catch (Exception exc) {
+            System.err.println("Error: " + exc.getMessage());
         }
+    }
 
-        scanner.close();
+    private static void printCsvColumns(File file) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String headerFile = reader.readLine();
+
+            if (headerFile != null) {
+                String[] columns = headerFile.split(",");
+
+                System.out.println("Available columns:");
+                System.out.println("+----------------------+");
+                for (String column : columns) {
+                    System.out.printf("| %-20s |\n", column.trim().toUpperCase());
+                }
+                System.out.println("+----------------------+");
+            } else {
+                System.out.println("The CSV file is empty.");
+            }
+        } catch (IOException exc) {
+            System.err.println("Error reading the file: " + exc.getMessage());
+        }
     }
 }
