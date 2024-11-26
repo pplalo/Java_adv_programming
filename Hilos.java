66231b0ca26506b5e6b4f4bf430670ab8b39feb5
pplalo <jose.eduardo.castagno@gmail.com>
2024-11-26 13:02:26 -0600

    Adjust thread pool size based on available CPUs for improved performance

--- Hilos.java
+++ Hilos.java
@@ -3,7 +3,10 @@ import java.util.concurrent.Executors;
 
 public class Hilos{
     public static void main(String[] args) {
-        ExecutorService pool = Executors.newFixedThreadPool(5);
+        int CPUs = Runtime.getRuntime().availableProcessors();
+        int nThreads = CPUs * 4;
+        
+        ExecutorService pool = Executors.newFixedThreadPool(nThreads);
 
         for (int i = 0; i < 10; i++) {
             pool.execute(new Runnable() {
