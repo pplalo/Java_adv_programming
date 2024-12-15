3a5b1a95200a926e2cce4c14a5605f49c10f1599
José E. Castaño <110692825+pplalo@users.noreply.github.com>
2024-12-14 23:47:47 -0600

    Update CsvReader.java

--- CsvReader.java
+++ CsvReader.java
@@ -13,38 +13,38 @@ public class CsvReader {
     // Iniciar la CLI para procesar el archivo CSV
     public static void initiateCsvCli() {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("\u001B[33m¡Bienvenido al Procesador de CSV!\u001B[0m");
-            System.out.println("\u001B[33mIngresa la ruta donde se encuentra el archivo CSV:\u001B[0m");
+            System.out.println("\u001B[33mWelcome!\u001B[0m");
+            System.out.println("\u001B[33mEnter the path where the CSV file is located:\u001B[0m");
             
             String filePath = scanner.nextLine();
             File file = new File(filePath);
 
             if (file.exists() && file.isFile() && filePath.endsWith(".csv")) {
-                System.out.println("\u001B[32mArchivo CSV encontrado: " + file.getAbsolutePath() + "\u001B[0m");
+                System.out.println("\u001B[32m CSV file found: " + file.getAbsolutePath() + "\u001B[0m");
                 
                 printCsvColumns(file);
                 
                 List<String[]> data = readCSV(filePath);
                 
                 if (data.isEmpty()) {
-                    System.out.println("Error al leer el archivo CSV");
+                    System.out.println("Error reading CSV file");
                     return;
                 }
                 
-                System.out.println("Columnas disponibles:");
+                System.out.println("Available columns:");
                 String[] headers = data.get(0);
                 for (int i = 0; i < headers.length; i++) {
                     System.out.println(i + ": " + headers[i]);
                 }
                 
-                System.out.print("Seleccione el índice de la columna para analizar: ");
+                System.out.print("Select index to be analyzed: ");
                 int columnIndex = scanner.nextInt();
                 
                 scanner.nextLine();
 
         
                 if (columnIndex < 0 || columnIndex >= headers.length) {
-                    System.out.println("Índice de columna inválido.");
+                    System.out.println("Invalid column index");
                     return;
                 }
                 List<Double> columnData = new ArrayList<>();
@@ -53,7 +53,7 @@ public class CsvReader {
                     try {
                         columnData.add(Double.valueOf(data.get(i)[columnIndex]));
                     } catch (NumberFormatException e) {
-                        System.out.println("No se encontró un valor númerico en el renglón" + (i + 1) + ", omitiendo.");
+                        System.out.println("No numeric value was found in the row" + (i + 1) + ", omitiendo.");
                     }
                 }
 
@@ -71,33 +71,33 @@ public class CsvReader {
                     // File analysisFile = new File("resultados/analisis_estadistico.txt");
                     // generarAnalisisEstadistico(subfiles, analysisFile);
                 } else {
-                    System.out.println("\u001B[31mNo se encontraron subarchivos para procesar.\u001B[0m");
+                    System.out.println("\u001B[31mNo subfiles were found to process.\u001B[0m");
                 }
 
             } else {
-                System.out.println("\u001B[31mError: Archivo no encontrado o no es un archivo CSV válido.\u001B[0m");
+                System.out.println("\u001B[31mError: File not found or not a valid CSV file.\u001B[0m");
             }
         } catch (Exception exc) {
             System.err.println("\u001B[31mError: " + exc.getMessage() + "\u001B[0m");
         }
     }
 
-    // Mostrar las columnas disponibles en el CSV
+    // Display available columns in CSV
     private static void printCsvColumns(File file) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String header = reader.readLine();
             if (header != null) {
                 String[] columns = header.split(",");
-                System.out.println("\u001B[34mColumnas disponibles:\u001B[0m");
+                System.out.println("\u001B[34mAvailable columns:\u001B[0m");
                 for (int i = 0; i < columns.length; i++) {
                     System.out.println((i + 1) + ". " + columns[i].trim());
                 }
             
             } else {
-                System.out.println("\u001B[31mEl archivo CSV está vacío.\u001B[0m");
+                System.out.println("\u001B[31mCSV file.\u001B[0m");
             }
         } catch (IOException e) {
-            System.err.println("\u001B[31mError al leer el archivo: " + e.getMessage() + "\u001B[0m");
+            System.err.println("\u001B[31mError reading file: " + e.getMessage() + "\u001B[0m");
         }
     }
 
@@ -110,7 +110,7 @@ public class CsvReader {
                 data.add(values);
             }
         } catch (IOException e) {
-            System.out.println("Error al leer el archivo CSV: " + e.getMessage());
+            System.out.println("Error reading CSV file: " + e.getMessage());
         }
         return data;
     }
@@ -120,7 +120,7 @@ public class CsvReader {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String header = reader.readLine();
             if (header == null) {
-                System.out.println("\u001B[31mEl archivo CSV está vacío.\u001B[0m");
+                System.out.println("\u001B[31mCSV file is empty.\u001B[0m");
                 return;
             }
 
